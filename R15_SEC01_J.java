fb404ce87e0c42214cf9fc7a15c408dc1c61d2af
Scientificmagic <thechanman.potato@gmail.com>
2021-10-18 16:44:34 -0400

    changed choice vulnerabilities

--- R15_SEC01_J.java
+++ R15_SEC01_J.java
@@ -1,18 +1,16 @@
-public void changePassword(String currentPassword, String newPassword) {
-	final FileInputStream f[] = { null };
-
-	AccessController.doPrivileged(new PrivilegedAction() {
-		public Object run() {
-			try {
-				String passwordFile = System.getProperty("user.dir") + File.separator + "PasswordFileName";
-				f[0] = new FileInputStream(passwordFile);
-				// Check whether oldPassword matches the one in the file
-				// If not, throw an exception
-				System.loadLibrary("authentication");
-			} catch (FileNotFoundException cnf) {
-				// Forward to handler
+private void privilegedMethod(final String filename)
+		throws FileNotFoundException {
+	try {
+		FileInputStream fis =
+			(FileInputStream) AccessController.doPrivileged(
+				new PrivilegedExceptionAction() {
+			public FileInputStream run() throws FileNotFoundException {
+				return new FileInputStream(filename);
 			}
-			return null;
 		}
-	}); // End of doPrivileged()
+	);
+	// Do something with the file and then close it
+	} catch (PrivilegedActionException e) {
+		// Forward to handler
+	}
 }
